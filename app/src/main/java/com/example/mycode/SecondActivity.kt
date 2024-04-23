047ef98dadfa1eaaeb016e330c14b8ec1cf67276
UserShahnoza <oliftaevashahnoza08@gmail.com>
2024-04-23 14:30:27 +0300

    new class

---
+++ app/src/main/java/com/example/mycode/SecondActivity.kt
@@ -0,0 +1,17 @@
+package com.example.mycode
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class SecondActivity : AppCompatActivity() {
+    private var button:Button?=null
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_second)
+//        button=findViewById<Button>(R.id.go)
+
+    }
+}
---
+++ app/src/main/java/com/example/mycode/newScreen.kt
@@ -0,0 +1,11 @@
+package com.example.mycode
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class newScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_screen)
+    }
+}
